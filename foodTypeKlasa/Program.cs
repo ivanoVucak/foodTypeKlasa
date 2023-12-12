3ba13c8fcbb3f71578d24a000e82641d1f43494c
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-12-12 10:30:19 +0100

    Add files via upload

---
+++ foodTypeKlasa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodTypeKlasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foodType foodType = new foodType();
+
+            foodType.setName("banana");
+            foodType.setProtein(4);
+            foodType.setCarbs(93);
+            foodType.setFat(3);
+
+            foodType foodType2 = new foodType("jagoda", 1, 1, 1);
+            
+
+            Console.WriteLine(foodType.ToString());
+            Console.WriteLine(foodType2.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ foodTypeKlasa/foodType.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodTypeKlasa
+{
+    internal class foodType
+    {
+        string name;
+        int protein, carbs, fat;
+
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void setProtein(int protein)
+        {
+            this.protein = protein;
+        }
+        public int GetProtein()
+        {
+            return this.protein;
+        }
+
+        public void setCarbs(int carbs)
+        {
+            this.carbs = carbs;
+        }
+        public int GetCarbs()
+        {
+            return this.carbs;
+        }
+
+        public void setFat(int fat)
+        {
+            this.fat = fat;
+        }
+        public int GetFat()
+        {
+            return this.fat;
+        }
+
+
+        public override string ToString()
+        {
+            string ispis = "Name: " + this.GetName() + "\n"
+                + "Protein: " + this.GetProtein() + "%\n"
+                + "Carbs: " + this.GetCarbs() + "%\n"
+                + "Fat: " + this.GetFat() + "%\n";
+            return ispis;
+        }
+
+
+
+        public foodType(String name, int protein, int carbs, int fat)
+        {
+            this.name = name;
+            this.protein = protein;
+            this.carbs = carbs;
+            this.fat = fat;
+        }
+        public foodType() { }
+    }
+}
